2272ff83ce7cea9103878c3720a2ce8988001bb0
WovenCoast <wovencoast14782@techie.com>
2020-04-23 13:51:17 +0500

    Status now shows current version

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   async exec(client) {
     const statuses = [
@@ -8,7 +10,8 @@ module.exports = {
     let currentStatus = 0;
     function setStatus() {
       const presence = client.util.getRandom(client.presences)(client);
-      client.user.setPresence({ activity: { name: `${presence} | ${client.util.getRandom(client.prefix)}help`, type: "WATCHING" }, status: statuses[currentStatus] });
+      const version = require(path.join(require.main.path, 'package.json')).version;
+      client.user.setPresence({ activity: { name: `${presence} | ${client.util.getRandom(client.prefix)}help | v${version}`, type: "WATCHING" }, status: statuses[currentStatus] });
       currentStatus = client.util.randomValue(0, statuses.length - 1);
     }
     console.log(`Ready as ${client.user.tag}!`);
