8f86d44629cc828c63576fa8e7bc15492be3fbac
WovenCoast <wovencoast14782@techie.com>
2020-04-23 13:57:43 +0500

    Frick, floats

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -11,7 +11,7 @@ module.exports = {
     let page = 1;
     if (serverQueue.songs.length > 5) {
       if (args[0] && !isNaN(args[0])) page = parseInt(args[0]);
-      if ((serverQueue.songs.length / 5) < page) throw new Error(`Cannot view page ${page} from ${client.util.pluralify(serverQueue.songs.length / 5, "valid page")}!`);
+      if (Math.round(serverQueue.songs.length / 5) < page) throw new Error(`Cannot view page ${page} from ${client.util.pluralify(Math.round(serverQueue.songs.length / 5), "valid page")}!`);
       songs = serverQueue.songs.slice((page - 1) * 5, page * 5);
     }
     return message.channel.send(new Discord.MessageEmbed().setTimestamp().setColor(client.colors.info).setAuthor(`${message.author.tag} | Queue`, message.author.displayAvatarURL()).setTitle(`Now playing: ${parseSongName(serverQueue.songs[0].title, serverQueue.songs[0].author)}`).setDescription(songs.map((song, index) => `**${((page - 1) * 5) + (index + 1)}**: ${parseSongName(song.title, song.author)}`)).setFooter(`Page ${page} of ${Math.round(serverQueue.songs.length / 5)}`));
