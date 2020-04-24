d550148d7e237726a6e6583729281f16430738d8
WovenCoast <wovencoast14782@techie.com>
2020-04-24 16:07:00 +0500

    Its in utils not in the play command :facepalm:

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -13,9 +13,6 @@ module.exports = {
       if (Math.ceil(message.guild.music.songs.length / 5) < page) throw new Error(`Cannot view page ${page} from ${client.util.pluralify(Math.ceil(message.guild.music.songs.length / 5), "valid page")}!`);
       songs = message.guild.music.songs.slice((page - 1) * 5, page * 5);
     }
-    return message.channel.send(new Discord.MessageEmbed().setTimestamp().setColor(client.colors.info).setAuthor(`${message.author.tag} | Queue`, message.author.displayAvatarURL()).setTitle(`Now playing: ${parseSongName(message.guild.music.songs[0])}, playhead at ${client.util.convertDuration(message.guild.music.duration)}`).setDescription(songs.map((song, index) => `**${((page - 1) * 5) + (index + 1)}**: ${client.util.parseSongName(song)}`)).setFooter(`Page ${page} of ${Math.ceil(message.guild.music.songs.length / 5)}`));
+    return message.channel.send(new Discord.MessageEmbed().setTimestamp().setColor(client.colors.info).setAuthor(`${message.author.tag} | Queue`, message.author.displayAvatarURL()).setTitle(`Now playing: ${client.util.parseSongName(message.guild.music.songs[0])}, playhead at ${client.util.convertDuration(message.guild.music.duration)}`).setDescription(songs.map((song, index) => `**${((page - 1) * 5) + (index + 1)}**: ${client.util.parseSongName(song)}`)).setFooter(`Page ${page} of ${Math.ceil(message.guild.music.songs.length / 5)}`));
   }
-}
-function parseSongName(title, author) {
-  return title.includes(author) ? title.replace(author, `**${author}**`) : `**${title}** by *${author}*`
 }
