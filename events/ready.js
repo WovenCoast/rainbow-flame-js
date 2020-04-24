2bb0a6a38646a15d78077d52144f823839ae16b9
WovenCoast <wovencoast14782@techie.com>
2020-04-24 10:53:29 +0500

    Small refractoring

--- events/ready.js
+++ events/ready.js
@@ -2,15 +2,6 @@ const path = require('path');
 
 module.exports = {
   async exec(client) {
-    if ((await client.db.client.get(client.user.id, "restartTimestamp")) !== 0) {
-      const feedbackChannel = await client.db.client.get(client.user.id, "restartInvokedChannel");
-      if (!feedbackChannel) return;
-      const start = await client.db.client.get(client.user.id, "restartTimestamp");
-      const millis = Date.now() - start;
-      feedbackChannel.send(`:white_check_mark: Successfully restarted the bot in ${client.util.convertMs(millis)}!`);
-      await client.db.client.set(client.user.id, "restartTimestamp", 0);
-      await client.db.client.set(client.user.id, "restartInvokedChannel", "");
-    }
     const statuses = [
       "online",
       "idle",
@@ -24,6 +15,15 @@ module.exports = {
     }
     console.log(`Ready as ${client.user.tag}!`);
     setStatus();
-    setInterval(setStatus, 15000)
+    setInterval(setStatus, 15000);
+    if ((await client.db.client.get(client.user.id, "restartTimestamp")) !== 0) {
+      const feedbackChannel = await client.db.client.get(client.user.id, "restartInvokedChannel");
+      if (!feedbackChannel) return;
+      const start = await client.db.client.get(client.user.id, "restartTimestamp");
+      const millis = Date.now() - start;
+      feedbackChannel.send(`:white_check_mark: Successfully restarted the bot in ${client.util.convertMs(millis)}!`);
+      await client.db.client.set(client.user.id, "restartTimestamp", 0);
+      await client.db.client.set(client.user.id, "restartInvokedChannel", "");
+    }
   }
 }
