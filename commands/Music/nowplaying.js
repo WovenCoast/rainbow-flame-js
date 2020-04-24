e0c8193891b95640a3e3801214baf9a7e96ddbdf
WovenCoast <wovencoast14782@techie.com>
2020-04-24 16:15:30 +0500

    Revamped queue command and fixed np

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -7,6 +7,6 @@ module.exports = {
   async exec(client, message, args) {
     if (!message.guild.music.playing) throw new Error("This command only works when I'm in a voice channel, try using the `play` command!");
     const song = message.guild.music.songs[0];
-    return message.channel.send(`:arrow_forward: Now Playing: ${client.util.parseSongName(song.title, song.author)}, playhead at ${client.util.convertDuration(message.guild.music.duration)}`)
+    return message.channel.send(`:arrow_forward: Now Playing: ${client.util.parseSongName(song)}, playhead at ${client.util.convertDuration(message.guild.music.duration)}`)
   }
 }
