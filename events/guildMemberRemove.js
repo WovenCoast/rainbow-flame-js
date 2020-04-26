eef1fc2e4ada67a113c8db56370b3946479634f6
WovenCoast <wovencoast14782@techie.com>
2020-04-26 23:13:25 +0500

    Fixed leave messages

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js');
 module.exports = {
   name: "guildMemberAdd",
   async exec(client, member) {
-    const welcomeMessages = (await client.db.guild.get(member.guild.id, "welcomeMessages"));
+    const leaveMessages = (await client.db.guild.get(member.guild.id, "leaveMessages"));
     const memberLogChannel = await client.db.guild.get(member.guild.id, "memberLogs");
     if (!memberLogChannel) return;
-    memberLogChannel.send(new Discord.MessageEmbed().setTimestamp().setColor(client.colors.success).setAuthor(`${member.guild.name} | New Member`, member.guild.iconURL({ dynamic: true })).setDescription(client.util.getRandom(welcomeMessages).replace(/{(.*)}/gi, (match, property) => `**${client.templates[property](member)}**`)).setThumbnail(member.user.displayAvatarURL({ dynamic: true })).setFooter(`In other words, welcome to ${member.guild.name}! You are the ${member.guild.memberCount}th member!`));
+    memberLogChannel.send(new Discord.MessageEmbed().setTimestamp().setColor(client.colors.error).setAuthor(`${member.guild.name} | Lost Member`, member.guild.iconURL({ dynamic: true })).setDescription(client.util.getRandom(leaveMessages).replace(/{(.*)}/gi, (match, property) => `**${client.templates[property](member)}**`)).setThumbnail(member.user.displayAvatarURL({ dynamic: true })).setFooter(`In other words, good bye ${member.user.tag}. Now there's only ${member.guild.memberCount} to enjoy ${member.guild.name}`));
   }
 }
