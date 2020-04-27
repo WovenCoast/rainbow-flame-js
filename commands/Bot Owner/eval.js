baf9c1f73d5e654832a2528f54dedeaac350bb8e
WovenCoast <wovencoast14782@techie.com>
2020-04-27 15:07:21 +0500

    Fixed some more things

--- commands/Bot Owner/eval.js
+++ commands/Bot Owner/eval.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js');
-const util = require('util');
-const haste = require('hastebin-gen');
+const Discord = require("discord.js");
+const util = require("util");
+const haste = require("hastebin-gen");
 
 module.exports = {
   name: "eval",
@@ -9,23 +9,31 @@ module.exports = {
   usage: "{prefix}eval <...expression:string>",
   cooldown: 2,
   async exec(client, message, args) {
-    if (!client.owners.includes(message.author.id)) throw new Error("You don't have enough permissions to evaluate a JavaScript command on me");
-    if (!args.join(' ')) {
-      return message.channel.send('Nothing to be evaled')
+    if (!client.owners.includes(message.author.id))
+      throw new Error(
+        "You don't have enough permissions to evaluate a JavaScript command on me"
+      );
+    if (!args.join(" ")) {
+      return message.channel.send("Nothing to be evaled");
     }
     var evaled;
     let color = client.colors.info;
     let millis = 0;
     const start = Date.now();
     try {
-      evaled = eval(args.join(' '));
+      evaled = eval(args.join(" "));
       millis = Date.now() - start;
       if (evaled instanceof Promise) evaled = await evaled;
       if (typeof evaled != "string") {
         evaled = util.inspect(evaled);
       }
       if (evaled.length > 1024) {
-        evaled = await haste(evaled.replace(client.token, "no cyka blyat"), { url: "http://haste.wovencoast.me", extension: "log" }).catch(err => { throw err });
+        evaled = await haste(evaled.replace(client.token, "no cyka blyat"), {
+          url: "http://haste.wovencoast.me",
+          extension: "log"
+        }).catch(err => {
+          throw err;
+        });
       }
     } catch (e) {
       evaled = e.stack;
@@ -34,12 +42,25 @@ module.exports = {
       }
       color = client.colors.error;
     }
-    message.channel.send(new Discord.MessageEmbed()
-      .setTimestamp()
-      .setColor(color)
-      .setFooter(`Evaluated in ${client.util.convertMs(millis)} (${millis}ms)`)
-      .setAuthor(`${message.author.id} | Eval`)
-      .addField('Input', `\`\`\`${args.join(' ')}\`\`\``)
-      .addField('Output', `${evaled.startsWith("http") ? "" : "\`\`\`"}${evaled.replace(client.token, "no cyka blyat")}${evaled.startsWith("http") ? "" : "\`\`\`"}`))
+    message.channel.send(
+      new Discord.MessageEmbed()
+        .setTimestamp()
+        .setColor(color)
+        .setFooter(
+          `Evaluated in ${client.util.convertMs(millis)} (${millis}ms)`
+        )
+        .setAuthor(
+          `${message.author.tag} | Eval`,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .addField("Input", `\`\`\`${args.join(" ")}\`\`\``)
+        .addField(
+          "Output",
+          `${evaled.startsWith("http") ? "" : "```"}${evaled.replace(
+            client.token,
+            "no cyka blyat"
+          )}${evaled.startsWith("http") ? "" : "```"}`
+        )
+    );
   }
-}
+};
