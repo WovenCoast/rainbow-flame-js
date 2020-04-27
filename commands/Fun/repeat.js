cabb4ae768e7bece9685728fbb851cb424dd97b9
WovenCoast <wovencoast14782@techie.com>
2020-04-27 15:00:31 +0500

    Made util support upto 1000 repeats

--- commands/Fun/repeat.js
+++ commands/Fun/repeat.js
@@ -6,8 +6,15 @@ module.exports = {
   async exec(client, message, args) {
     if (isNaN(args[0]))
       throw new Error(`\`${args[0]}\` is not a valid amount!`);
-    return message.channel.send(
-      (args.slice(1, args.length).join(" ") + "\n").repeat(parseInt(args[0]))
+    if (parseInt(args[0]) > 1000)
+      throw new Error("You need to specify a value between 0 and 1000!");
+    const str = (args.slice(1, args.length).join(" ") + "\n").repeat(
+      parseInt(args[0])
+    );
+    const y = 2000;
+    const x = args.slice(1, args.length).join(" ").length;
+    return await Promise.all(
+      client.util.chunk((y / x - 1) * x, str).map(s => message.channel.send(s))
     );
   }
 };
