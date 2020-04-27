49553af8fbe9c0298eb7ab66bdcf25a884d78461
WovenCoast <wovencoast14782@techie.com>
2020-04-27 15:17:19 +0500

    Made yet more changes

--- commands/Fun/repeat.js
+++ commands/Fun/repeat.js
@@ -6,13 +6,20 @@ module.exports = {
   async exec(client, message, args) {
     if (isNaN(args[0]))
       throw new Error(`\`${args[0]}\` is not a valid amount!`);
-    if (parseInt(args[0]) > 1000)
+    if (parseInt(args[0]) > 1000 || parseInt(args[0]) < 0)
       throw new Error("You need to specify a value between 0 and 1000!");
-    const str = (args.slice(1, args.length).join(" ") + "\n").repeat(
+    const x = args.slice(1, args.length).join(" ").length;
+    if (
+      !message.member.hasPermission("MANAGE_MESSAGES") &&
+      parseInt(args[0]) > 50
+    )
+      throw new Error(
+        "Only people who can delete the messages can send over 50 repeats!"
+      );
+    const str = (args.slice(1, args.length).join(" ") + " ").repeat(
       parseInt(args[0])
     );
     const y = 2000;
-    const x = args.slice(1, args.length).join(" ").length;
     return await Promise.all(
       client.util.chunk((y / x - 1) * x, str).map(s => message.channel.send(s))
     );
