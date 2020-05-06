18051a4f491d4e40f03e86ea1a348a8bbc19bb0c
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-06 17:52:57 +0530

    Update gif.js

--- commands/Misc/gif.js
+++ commands/Misc/gif.js
@@ -9,7 +9,6 @@ module.exports = {
         fetch(`https://api.tenor.com/v1/random?key=LJIHBE2W7RAB&q=${args}&limit=1`)
         res => res.json()
         json => message.say(json.results[0].url)
-             
         return;
     }
 }
