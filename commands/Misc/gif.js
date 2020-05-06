516567995f0d7a73b71a3fc1da001a0e22494dc4
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-06 16:00:48 +0530

    Update gif.js

--- commands/Misc/gif.js
+++ commands/Misc/gif.js
@@ -7,12 +7,9 @@ module.exports = {
     desc: "Sends a gif",
     async exec(client, message, args) {
         fetch(`https://api.tenor.com/v1/random?key=LJIHBE2W7RAB&q=${args}&limit=1`)
-            .then(res => res.json())
-            .then(json => message.say(json.results[0].url))
-            .catch(e => {
-                message.say('Failed to find a gif that matched your query');
-                return console.error(e);
-            });
+        res => res.json()
+        json => message.say(json.results[0].url)
+             
         return;
     }
 }
