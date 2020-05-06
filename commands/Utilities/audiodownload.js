b192ee0a275df77bff8d9c03d68fbb74a5cc548c
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-06 12:24:03 +0530

    :+1:

---
+++ commands/Utilities/audiodownload.js
@@ -0,0 +1,20 @@
+const search = require('yt-search');
+const ytdl = require("ytdl-core");
+const tinyurl = require('tinyurl');
+
+module.exports = {
+	name: "audiodownload",
+	aliases: ["adownload","mp3"],
+	desc: "Send a link from which you can download the audio from a YouTube video",
+	async exec(client, message, args) {
+		const res = await search(args.join(" "));
+		const info = await ytdl.getInfo(res.videos[0].videoId)
+		const format = ytdl.chooseFormat(info.formats, 'highestaudio');
+		if (format) {
+			const shortenedUrl = await tinyurl.shorten(format.url);
+			message.channel.send(`*${info.title}* by **${info.author.name}**: <${shortenedUrl}>`);
+		} else {
+			message.channel.send(`*${info.title}* by **${info.author.name}**: Cannot be downloaded :(`);
+		}
+	}
+}
---
+++ commands/Utilities/videodownload.js
@@ -0,0 +1,20 @@
+const search = require('yt-search');
+const ytdl = require("ytdl-core");
+const tinyurl = require('tinyurl');
+
+module.exports = {
+	name: "videodownload",
+	aliases: ["vdownload","mp4"],
+	desc: "Send a link from which you can download the YouTube video",
+	async exec(client, message, args) {
+		const res = await search(args.join(" "));
+		const info = await ytdl.getInfo(res.videos[0].videoId)
+		const format = ytdl.chooseFormat(info.formats, 'highestvideo');
+		if (format) {
+			const shortenedUrl = await tinyurl.shorten(format.url);
+			message.channel.send(`*${info.title}* by **${info.author.name}**: <${shortenedUrl}>`);
+		} else {
+			message.channel.send(`*${info.title}* by **${info.author.name}**: Cannot be downloaded :(`);
+		}
+	}
+}
