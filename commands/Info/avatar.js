c26b5419568406b490548a6f724ed2e38d97096d
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-06 15:46:03 +0530

    Revert "yare yare daze"
    This reverts commit e261b6c2980cf2b7f613da677fb4e60f67be693d.

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -6,9 +6,6 @@ module.exports = {
     desc: "Sends the avatar of the user mentioned",
     async exec(client, message, args) {
         const user = client.util.parseUser(args.join(" "));
-        if(user == null){
-            user = message.user;
-        }
         return message.channel.send(new Discord.MessageEmbed()
         .setImage(user.avatarURL({ dynamic: true }))
         .setColor(client.colors.info).setAuthor(`${user.tag} | Avatar`).setTitle(`(Download Avatar)[${user.displayAvatarURL({ dynamic: true })}]`));
