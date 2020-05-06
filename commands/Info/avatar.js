056266aa1a5a50629444566886a1df317d5f1ed8
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-06 15:25:02 +0530

    Update avatar.js

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: [],
     desc: "Sends the avatar of the user mentioned",
     async exec(client, message, args) {
-        var user = message.mentions.users.first();
+        var user = args;
         let embed = new Discord.RichEmbed()
         .setImage(user.avatarURL)
         .setColor('#275BF0')
