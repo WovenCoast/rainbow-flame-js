713f3308a8fbb2a6d7798952311998cfdfadf0e6
WovenCoast <wovencoast14782@techie.com>
2020-05-06 15:13:40 +0500

    Idk

--- commands/Bot Owner/reload.js
+++ commands/Bot Owner/reload.js
@@ -1,33 +1,49 @@
-const Discord = require('discord.js');
-const path = require('path');
+const Discord = require("discord.js");
+const path = require("path");
 const childProcess = require("child_process");
 
 module.exports = {
-	name: "reload",
-	aliases: [],
-	desc: "Reload everything in the bot",
-	async exec(client, message, args) {
-		if (!client.owners.includes(message.author.id)) throw new Error("You don't have enough permissions to reload my commands");
-		const start = Date.now();
-		await childProcess.exec(
+  name: "reload",
+  aliases: [],
+  desc: "Reload everything in the bot",
+  async exec(client, message, args) {
+    if (!client.owners.includes(message.author.id))
+      throw new Error(
+        "You don't have enough permissions to reload my commands"
+      );
+    const start = Date.now();
+    await childProcess.exec(
       "git pull && npm i",
       async (err, stdout, stderr) => {
         if (err)
           await message.channel.send(
             `Something went wrong:-\`\`\`${stderr}\`\`\`Restarting anyways...`
           );
-	      
-		client.commands.forEach(command => {
-			delete require.cache[require.resolve(path.join(require.main.path, client.customOptions.commands, command.category, command.name + ".js"))];
-		})
-		client.removeAllListeners();
-		client.commands = new Discord.Collection();
-		client.aliases = new Discord.Collection();
-		client._readCommands(client.customOptions.commands);
-		client._readEvents(client.customOptions.events);
-		const millis = Date.now() - start;
-		message.channel.send(`:white_check_mark: Successfully reloaded all the commands in ${client.util.convertMs(millis)} (${millis}ms)!`);
+
+        client.commands.forEach((command) => {
+          delete require.cache[
+            require.resolve(
+              path.join(
+                require.main.path,
+                client.customOptions.commands,
+                command.category,
+                command.name + ".js"
+              )
+            )
+          ];
+        });
+        client.removeAllListeners();
+        client.commands = new Discord.Collection();
+        client.aliases = new Discord.Collection();
+        client._readCommands(client.customOptions.commands);
+        client._readEvents(client.customOptions.events);
+        const millis = Date.now() - start;
+        message.channel.send(
+          `:white_check_mark: Successfully reloaded all the commands in ${client.util.convertMs(
+            millis
+          )} (${millis}ms)!`
+        );
       }
     );
-	}
-}
+  },
+};
--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -1,13 +1,19 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
-    name: "avatar",
-    aliases: [],
-    desc: "Sends the avatar of the user mentioned",
-    async exec(client, message, args) {
-        const user = client.util.parseUser(args.join(" "));
-        return message.channel.send(new Discord.MessageEmbed()
-        .setImage(user.avatarURL({ dynamic: true }))
-        .setColor(client.colors.info).setAuthor(`${user.tag} | Avatar`).setTitle(`(Download Avatar)[${user.displayAvatarURL({ dynamic: true })}]`));
-    }
-}
+  name: "avatar",
+  aliases: [],
+  desc: "Sends the avatar of the user mentioned",
+  async exec(client, message, args) {
+    const user = client.util.parseUser(args.join(" "));
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setImage(user.displayAvatarURL({ dynamic: true }))
+        .setColor(client.colors.info)
+        .setAuthor(`${user.tag} | Avatar`)
+        .setTitle(
+          `(Download Avatar)[${user.displayAvatarURL({ dynamic: true })}]`
+        )
+    );
+  },
+};
