2108e4243c794c3975c1471c5ea0137195b1cb02
WovenCoast14782 <44475239+WovenCoast@users.noreply.github.com>
2020-05-06 15:00:19 +0500

    Literally changing Rich to Message

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -6,7 +6,7 @@ module.exports = {
     desc: "Sends the avatar of the user mentioned",
     async exec(client, message, args) {
         const user = client.util.parseUser(args.join(" "));
-        return message.channel.send(new Discord.RichEmbed()
+        return message.channel.send(new Discord.MessageEmbed()
         .setImage(user.avatarURL({ dynamic: true }))
         .setColor(client.colors.info).setAuthor(`${user.tag} | Avatar`).setTitle(`(Download Avatar)[${user.displayAvatarURL({ dynamic: true })}]`));
     }
