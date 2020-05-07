2d634c4c991bada5110e085fa4c39db3c7696f3c
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-07 10:45:29 +0530

    Update gif.js

--- commands/Misc/gif.js
+++ commands/Misc/gif.js
@@ -8,7 +8,7 @@ module.exports = {
     async exec(client, message, args) {
         fetch(`https://api.tenor.com/v1/random?key=LJIHBE2W7RAB&q=${args}&limit=1`)
         res => res.json()
-        json => message.say(json.results[0].url)
+        message.say(res.json.results[0].url)
         return;
     }
 }
