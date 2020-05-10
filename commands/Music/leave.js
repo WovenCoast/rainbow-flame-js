86661665dab4655105acdd76b3f251b2f842e690
WovenCoast <wovencoast14782@techie.com>
2020-05-10 17:57:16 +0500

    Setup perms for leave

--- commands/Music/leave.js
+++ commands/Music/leave.js
@@ -4,6 +4,8 @@ module.exports = {
   desc: "Leave the voice channel",
   async exec(client, message, args) {
     if (!message.guild.me.voice.channel) throw new Error("I am not connected to any channel of this guild!");
+    if (!message.member.voice.channel) throw new Error("You need to be connected to a voice channel!");
+    if (message.guild.me.voice.channel.id !== message.member.voice.channel.id) throw new Error("You need to be in the same channel as I am!");
     message.guild.me.voice.channel.leave();
     message.guild.music.resetValues();
     return message.channel.send(":white_check_mark: Left the voice channel successfully!");
