5c5aa3002da0e00fe7b3a71c31e6a01f7220f952
WovenCoast <wovencoast14782@techie.com>
2020-05-12 17:46:16 +0500

    uhh

--- commands/Fun/repeat.js
+++ commands/Fun/repeat.js
@@ -21,7 +21,7 @@ module.exports = {
     );
     return await Promise.all(
       client.util
-        .chunk((1999 / (x - 1)) * x + 1, str)
+        .chunk((1900 / (x - 1)) * x + 1, str)
         .map((s) => message.channel.send(s))
     );
   },
