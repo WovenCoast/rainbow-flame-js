e69528667cc0d8d1cf204f5dc1ec034c48bfc1b7
Madhav Kapa <madhavkapa16@gamil.com>
2020-05-13 12:05:48 +0530

    ;-; ;-; ;-; ;-; ;-; ;-; ;-; ;-; ;-; ;-; ;-;

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -5,10 +5,10 @@ module.exports = {
 	aliases: ["exile"],
 	desc: "BAN!!!!!!",
 	async exec(client, message, args) {
-		//let banuser=args[0]
+		let banuser=args[0]
 		message.channel.send("hi")
-		/*user.ban(banuser);
-		const banEmbed = new Discord()
+		user.ban(banuser);
+		/*const banEmbed = new Discord()
 			.addField('Banned:', banuser)
 			.setColor('#420626');
 		message.channel.send(banEmbed);*/
