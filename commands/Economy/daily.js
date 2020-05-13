27a2cee0a4bed0aa3359647391117c79992b51a3
Madhav Kapa <madhavkapa16@gamil.com>
2020-05-13 18:29:18 +0530

    //WIP

--- commands/Economy/daily.js
+++ commands/Economy/daily.js
@@ -5,6 +5,6 @@ module.exports = {
     usage: "???",
     async exec(client, message, args) {
         await client.db.member._set(message.guild.id + message.author.id, "wallet", (await client.db.member.get(message.guild.id + message.author.id, "wallet")) + 100);
-
+        message.channel.send("You got ur daily. :smile:")
     }
   }
