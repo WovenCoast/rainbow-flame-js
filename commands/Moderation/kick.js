23d776c72fb630b3a407a0597a6befd367b902d1
Madhav Kapa <madhavkapa16@gamil.com>
2020-05-13 19:27:19 +0530

    ???

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -7,9 +7,6 @@ module.exports = {
   desc: "KICK!!!!!!!",
   async exec(client, message, args) {
     const reason = args.slice(2).join(" ") || "No reason at all";
-    const ms = Math.round(moment() - moment().relativeTime("-" + args[1]));
-    if (!ms) throw new Error(`${args[1]} is not a valid timestamp!`);
-    const days = ms / 8.64e7;
     const member = client.util.parseMember(message.guild, args[0]);
     if (!member) throw new Error(`${args} couldn't be resolved to a member!`);
     if (!message.member.hasPermission("KICK_MEMBERS"))
@@ -26,7 +23,6 @@ module.exports = {
         .setAuthor(`${message.author.tag} | The Kick`)
         .setTitle(`Kicked ${member.user.tag}`)
         .addField("Reason", reason)
-        .addField("Duration", client.util.convertMs(ms))
     );
   },
 };
