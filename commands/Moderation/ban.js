df3fa61e904d768eb6990f95f3abe852dc68aaf3
WovenCoast <wovencoast14782@techie.com>
2020-05-13 13:39:22 +0500

    Hopefully makes the ban hammer a thing

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,16 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const moment = require("relative-time-parser");
 
 module.exports = {
-	name: "ban",
-	aliases: ["exile"],
-	desc: "BAN!!!!!!",
-	async exec(client, message, args) {
-		let banmember=args[0]
-		message.channel.send("hi")
-		member.ban(banmember);
-		const banEmbed = new Discord.MessageEmbed()
-			.addField('Banned:', banmember)
-			.setColor('#420626');
-		message.channel.send(banEmbed);
-	}
-}
+  name: "ban",
+  aliases: ["exile"],
+  desc: "BAN HAMMER!!!!!!",
+  async exec(client, message, args) {
+    const reason = args.slice(2).join(" ") || "No reason at all";
+    const ms = Math.round(moment() - moment().relativeTime("-" + args[1]));
+    if (!ms) throw new Error(`${args[1]} is not a valid timestamp!`);
+    const days = ms / 8.64e7;
+    const member = client.util.parseMember(message.guild, args[0]);
+    if (!member) throw new Error(`${args} couldn't be resolved to a member!`);
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+      throw new Error("You don't have enough permissions to ban!");
+    if (!member.kickable)
+      throw new Error(
+        `I don't have enough permissions to ban ${member.user.tag}!`
+      );
+    await member.ban({ reason, days });
+    message.channel.send(
+      new Discord.MessageEmbed()
+        .setTimestamp()
+        .setColor(client.colors.info)
+        .setAuthor(`${message.author.tag} | Ban Hammer`)
+        .setTitle(`Banned ${member.user.tag}`)
+        .addField("Reason", reason)
+        .addField("Duration", client.util.convertMs(ms))
+    );
+  },
+};
