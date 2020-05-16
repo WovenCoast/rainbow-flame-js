e396e156a486b7cd5380d6c641cb225a2d53ba1a
WovenCoast <wovencoast14782@techie.com>
2020-05-16 15:35:32 +0500

    Lool

--- commands/Moderation/nick.js
+++ commands/Moderation/nick.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "nick",
-  aliases: ["getout"],
+  aliases: [],
   desc: "NICK!!!!!!!",
   usage: "{prefix}nick <victim:member> <newNick:string>",
   async exec(client, message, args) {
