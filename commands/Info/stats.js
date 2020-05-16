248641d1aa8ed158965487a17f79ce4b3d6bbe4f
WovenCoast <wovencoast14782@techie.com>
2020-05-16 21:42:36 +0500

    Ehh, this is cool

--- commands/Info/stats.js
+++ commands/Info/stats.js
@@ -3,7 +3,7 @@ const os = require("os");
 
 module.exports = {
   name: "stats",
-  aliases: ["info", "stat", "botstat", "status"],
+  aliases: ["info", "stat", "botstat", "status", "uptime"],
   desc: "View the stats of this bot",
   async exec(client, message, args) {
     return message.channel.send(
@@ -11,10 +11,7 @@ module.exports = {
         .setTimestamp()
         .setColor(client.colors.info)
         .setThumbnail(client.user.displayAvatarURL())
-        .setAuthor(
-          `${client.user.tag} | Stats`,
-          client.user.displayAvatarURL()
-        )
+        .setAuthor(`${client.user.tag} | Stats`, client.user.displayAvatarURL())
         .addField(
           "Software Info",
           `${client.user.username}: \`${client.version}\`\nNode: \`v${process.versions.node}\`\nDiscord.js: \`v${Discord.version}\`\nPlatform: \`${process.platform} (${process.arch})\``
@@ -34,5 +31,5 @@ module.exports = {
           `Users: \`${client.users.cache.size}\`\nGuilds: \`${client.guilds.cache.size}\`\nChannels: \`${client.channels.cache.size}\`\nCommands Executed: \`${client.commandStatus.exec}\`\nCommands Successful: \`${client.commandStatus.success}\`\nCommands Failed: \`${client.commandStatus.fail}\``
         )
     );
-  }
+  },
 };
