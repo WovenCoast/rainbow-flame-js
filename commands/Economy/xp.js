1b0d67966109d018524bdff8d41a9409890ef173
Madhav Kapa <madhavkapa16@gamil.com>
2020-05-16 22:00:37 +0530

    Added or fixed stuff (this is a template lol)

--- commands/Economy/xp.js
+++ commands/Economy/xp.js
@@ -21,7 +21,7 @@ module.exports = {
         .setTimestamp()
         .setColor(client.colors.info)
         .setAuthor(
-          `${message.author.tag} | Rank`,
+          `${message.user.tag} | Rank`,
           member.user.displayAvatarURL({ dynamic: true })
         )
         .setDescription(
